fbc0ea7292c5ba8ef54ba6d2352239343b5662d0
Alexey <57992874+av-fedorov@users.noreply.github.com>
2025-09-12 12:34:18 +0300

    Homework 10 v.01

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,11 +6,12 @@ class Program
     {
         string command;
         string name = "";
+        List<string> todoList = new List<string>();
 
         Console.WriteLine("Добро пожаловать в консольного бота :) \n");
         Console.WriteLine("Список доступных команд:");
-        Console.WriteLine(" /start \n /help \n /info \n /echo \n /exit");
-
+        Console.WriteLine(" /start \n /help \n /info \n /echo \n" +
+                          " /addtask \n /showtasks \n /removetask \n /exit");
         do
         {
             Console.WriteLine("\n---------------------------");
@@ -32,12 +33,15 @@ class Program
                     break;
                 case "/help":
                     Console.WriteLine("Список доступных команд:");
-                    Console.WriteLine(" /start  - начало работы с программой.");
-                    Console.WriteLine(" /help   - вывод описания по доступным командам.");
-                    Console.WriteLine(" /info   - техническая информация о программе.");
-                    Console.WriteLine(" /echo   - выводит в консоль текст, введенный после команд /echo. " +
-                                      "Например: /echo Hello World!");
-                    Console.WriteLine(" /exit   - выход из программы.");
+                    Console.WriteLine(" /start      - начало работы с программой.");
+                    Console.WriteLine(" /help       - вывод описания по доступным командам.");
+                    Console.WriteLine(" /info       - техническая информация о программе.");
+                    Console.WriteLine(" /echo       - выводит в консоль текст, введенный после команд /echo. " +
+                                                      "Например: /echo Hello World!");
+                    Console.WriteLine(" /addtask    - добавить задачу в список дел.");
+                    Console.WriteLine(" /showtasks  - отобразить задачи из списка дел.");
+                    Console.WriteLine(" /removetask - удалить задачу из списка дел.");
+                    Console.WriteLine(" /exit       - выход из программы.");
                     break;
                 case "/info":
                     Console.WriteLine("Версия программы: 0.1, дата создания: 19.08.2025.");
@@ -52,11 +56,50 @@ class Program
                         Console.WriteLine("Команда /echo не доступна, т.к. не задано имя пользователя. "
                                           + "Задайте имя используя команду /start.");
                     }
+                    break;
+                case "/addtask":
+                    Console.WriteLine("Введите описание задачи: ");
+                    todoList.Add(Console.ReadLine());
+                    Console.WriteLine("Задача добавлена.");
+                    break;
+                case "/showtasks":
+                    ListTasks(todoList);
+                    break;
+                case "/removetask":
+                    ListTasks(todoList);
 
+                    if (todoList.Count > 0)
+                    {
+                        Console.Write("Выберите номер задачи для удаления: ");
+                    
+                        int taskNumber = Convert.ToInt32(Console.ReadLine());   
+                        // планирую добавить exception после соответствующего урока :)
+                    
+                        if (taskNumber > 0 && taskNumber <= todoList.Count)
+                        {
+                            todoList.RemoveAt(taskNumber - 1);
+                            Console.WriteLine($"Задача #{taskNumber} удалена.");    
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Задача #{taskNumber} отсутствует в списке.");
+                        }
+                    }
                     break;
             }
         } while (command != "/exit");
 
         Console.WriteLine("Программа завершена, до свидания.");
+    }
+    
+    public static void ListTasks(List<string> todoList)
+    {
+        if (todoList.Count == 0)
+            Console.WriteLine("Список задач пуст.");
+        else
+        {
+            for (int i = 0; i < todoList.Count; i++)
+                Console.WriteLine($"{i + 1}. {todoList[i]}");    
+        }
     }
 }
