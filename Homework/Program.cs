6d2456124342360ba6b337dd2ea8e4c7760a8cf0
Alexey <57992874+av-fedorov@users.noreply.github.com>
2025-10-30 11:03:05 +0300

    Добавил исправления: 1) Вынес код команд в методы, создал глобальные переменные 2) Добавил доп. проверки

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,15 +1,16 @@
-﻿namespace Homework;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Homework;
 
 class Program
 {
+    private static string versionText = "Версия программы: 0.2.1, дата создания: 09.09.2025.";
+    private static List<string> todoList = new List<string>();
+    private static string command;
+    private static string name = "";
+    
     static void Main()
     {
-        string command;
-        string name = "";
-        List<string> todoList = new List<string>();
-        int taskNumber; 
-        bool exitRemoveFlag  = false;
-
         Console.WriteLine("Добро пожаловать в консольного бота :) \n");
         Console.WriteLine("Список доступных команд:");
         Console.WriteLine(" /start \n /help \n /info \n /echo \n" +
@@ -30,68 +31,25 @@ class Program
             switch (command)
             {
                 case "/start":
-                    Console.Write("Введите свое имя: ");
-                    name = Console.ReadLine();
+                    name = StartApp();
                     break;
                 case "/help":
-                    Console.WriteLine("Список доступных команд:");
-                    Console.WriteLine(" /start      - начало работы с программой.");
-                    Console.WriteLine(" /help       - вывод описания по доступным командам.");
-                    Console.WriteLine(" /info       - техническая информация о программе.");
-                    Console.WriteLine(" /echo       - выводит в консоль текст, введенный после команд /echo. " +
-                                      "Например: /echo Hello World!");
-                    Console.WriteLine(" /addtask    - добавить задачу в список дел.");
-                    Console.WriteLine(" /showtasks  - отобразить задачи из списка дел.");
-                    Console.WriteLine(" /removetask - удалить задачу из списка дел.");
-                    Console.WriteLine(" /exit       - выход из программы.");
+                    ShowHelp();
                     break;
                 case "/info":
-                    Console.WriteLine("Версия программы: 0.2, дата создания: 09.09.2025.");
+                    ShowInfo();
                     break;
-                case string cmd when cmd.IndexOf("/echo") >= 0:
-                    if (!string.IsNullOrEmpty(name) || !string.IsNullOrWhiteSpace(name))
-                    {
-                        Console.WriteLine(cmd.Substring("/echo".Length).Trim());
-                    }
-                    else
-                    {
-                        Console.WriteLine("Команда /echo не доступна, т.к. не задано имя пользователя. "
-                                          + "Задайте имя используя команду /start.");
-                    }
-
+                case string cmd when cmd.StartsWith("/echo"):
+                    Echo(cmd, name);
                     break;
                 case "/addtask":
-                    Console.WriteLine("Введите описание задачи: ");
-                    todoList.Add(Console.ReadLine());
-                    Console.WriteLine("\nЗадача добавлена.");
+                    AddTask();
                     break;
                 case "/showtasks":
-                    ListTasks(todoList);
+                    ShowTasks();
                     break;
                 case "/removetask":
-                    if (todoList.Count == 0)
-                        Console.WriteLine("Список задач пуст.");
-                    else
-                    {
-                        do
-                        {
-                            ListTasks(todoList);
-                            Console.Write("\nВыберите номер задачи для удаления: ");
-                            int.TryParse(Console.ReadLine(), out taskNumber);
-
-                            if (taskNumber > 0 && taskNumber <= todoList.Count)
-                            {
-                                todoList.RemoveAt(taskNumber - 1);
-                                Console.WriteLine($"Задача #{taskNumber} удалена.");
-                                exitRemoveFlag = true;
-                            }
-                            else
-                            {
-                                Console.WriteLine($"\nЗадача c выбранным номером отсутствует в списке." +
-                                                  "\nВведите корректный номер задачи.\n");
-                            }
-                        } while ((taskNumber <= 0 || taskNumber > todoList.Count) && todoList.Count > 0 && !exitRemoveFlag);
-                    }
+                    RemoveTask();
                     break;
             }
         } while (command != "/exit");
@@ -99,7 +57,37 @@ class Program
         Console.WriteLine("Программа завершена, до свидания.");
     }
 
-    public static void ListTasks(List<string> todoList)
+    public static String StartApp()
+    {
+        while (string.IsNullOrWhiteSpace(name))
+        {
+            Console.Write("Введите свое имя: ");
+            name = Console.ReadLine();
+        }
+
+        return name;
+    }
+    
+    public static void ShowInfo()
+    {
+        Console.WriteLine(versionText);
+    }
+    
+    public static void ShowHelp()
+    {
+        Console.WriteLine("Список доступных команд:");
+        Console.WriteLine(" /start      - начало работы с программой.");
+        Console.WriteLine(" /help       - вывод описания по доступным командам.");
+        Console.WriteLine(" /info       - техническая информация о программе.");
+        Console.WriteLine(" /echo       - выводит в консоль текст, введенный после команд /echo. " +
+                          "Например: /echo Hello World!");
+        Console.WriteLine(" /addtask    - добавить задачу в список дел.");
+        Console.WriteLine(" /showtasks  - отобразить задачи из списка дел.");
+        Console.WriteLine(" /removetask - удалить задачу из списка дел.");
+        Console.WriteLine(" /exit       - выход из программы.");
+    }
+    
+    public static bool ShowTasks()
     {
         if (todoList.Count == 0)
             Console.WriteLine("Список задач пуст.");
@@ -110,5 +98,62 @@ class Program
             for (int i = 0; i < todoList.Count; i++)
                 Console.WriteLine($"{i + 1}. {todoList[i]}");
         }
+        
+        return todoList.Count > 0;
+    }
+    
+    public static void AddTask()
+    {
+        string taskText = "";
+
+        while (string.IsNullOrWhiteSpace(taskText))
+        {
+            Console.WriteLine("Введите описание задачи: ");
+            taskText = Console.ReadLine();
+            todoList.Add(taskText);
+        }
+        
+        Console.WriteLine("\nЗадача добавлена.");
+    }
+
+    public static void RemoveTask()
+    {
+        if (ShowTasks())
+        {
+            int taskNumber;
+            bool exitRemoveFlag = false;
+            
+            do
+            {
+                Console.Write("\nВыберите номер задачи для удаления: ");
+                int.TryParse(Console.ReadLine(), out taskNumber);
+        
+                if (taskNumber > 0 && taskNumber <= todoList.Count)
+                {
+                    todoList.RemoveAt(taskNumber - 1);
+                    Console.WriteLine($"Задача #{taskNumber} удалена.");
+                    exitRemoveFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine($"\nЗадача c выбранным номером отсутствует в списке." +
+                                      "\nВведите корректный номер задачи.\n");
+                }
+            } while ((taskNumber <= 0 || taskNumber > todoList.Count) && todoList.Count > 0 && !exitRemoveFlag);
+        }
+    }
+    
+    public static void Echo(String cmd, String name)
+    {
+        if (!string.IsNullOrEmpty(name) || !string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine(cmd.Substring("/echo".Length).Trim());
+        }
+        else
+        {
+            Console.WriteLine("Команда /echo не доступна, т.к. не задано имя пользователя. "
+                              + "Задайте имя используя команду /start.");
+        }
+
     }
 }
