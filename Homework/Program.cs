0394c9731f81c07e0eb48639dd46f502d42ae39a
Alexey <57992874+av-fedorov@users.noreply.github.com>
2025-11-03 17:14:13 +0300

    Выполнил все пункты ДЗ.

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -2,62 +2,95 @@
 
 class Program
 {
-    private static string versionText = "Версия программы: 02.13.02, дата создания: 01.11.2025.";
+    private static string versionText = "Версия программы: 02.11.01, дата создания: 02.11.2025.";
     private static List<ToDoItem> todoList = new List<ToDoItem>();
     private static ToDoUser User = new ToDoUser(null);
     private static string command;
-
+    private static int taskCountLimit;
+    private static int taskLengthLimit;
+    
     static void Main()
     {
-        Console.WriteLine("Добро пожаловать в консольного бота :) \n");
-        Console.WriteLine("Список доступных команд:");
-        Console.WriteLine(" /start \n /help \n /info \n " +
-                          "/addtask \n /showtasks \n /showalltasks \n /completetask \n " +
-                          "/exit");
-
-        do
+        try
         {
-            Console.WriteLine("\n---------------------------");
-            Console.Write("Введите название команды: ");
-            command = Console.ReadLine();
+            Console.WriteLine("Добро пожаловать в консольного бота :) \n");
+            Console.WriteLine("Список доступных команд:");
+            Console.WriteLine(" /start \n /help \n /info \n " +
+                              "/addtask \n /showtasks \n /showalltasks \n /completetask \n " +
+                              "/exit");
 
-            Console.WriteLine("\nВы ввели: " + command + "\n");
-
-            switch (command)
+            do
             {
-                case "/start":
-                    StartApp();
-                    break;
-                case "/help":
-                    ShowHelp();
-                    break;
-                case "/info":
-                    ShowInfo();
-                    break;
-                case "/addtask":
-                    AddTask();
-                    break; 
-                case "/completetask":
-                    CompleteTask();
-                    break;
-                case "/showtasks":
-                    ShowTasks("Active");
-                    break;
-                case "/showalltasks":
-                    ShowTasks("All");
-                    break;
-            }
-        } while (command != "/exit");
+                Console.WriteLine("\n---------------------------");
+                Console.Write("Введите название команды: ");
+                command = Console.ReadLine();
 
-        Console.WriteLine("Программа завершена, до свидания.");
+                Console.WriteLine("\nВы ввели: " + command + "\n");
+
+                switch (command)
+                {
+                    case "/start":
+                        StartApp();
+                        break;
+                    case "/help":
+                        ShowHelp();
+                        break;
+                    case "/info":
+                        ShowInfo();
+                        break;
+                    case "/addtask":
+                        AddTask();
+                        break;
+                    case "/completetask":
+                        CompleteTask();
+                        break;
+                    case "/showtasks":
+                        ShowTasks("Active");
+                        break;
+                    case "/showalltasks":
+                        ShowTasks("All");
+                        break;
+                }
+            } while (command != "/exit");
+
+            Console.WriteLine("Программа завершена, до свидания.");
+        }
+
+        catch (TaskCountLimitException) {}
+
+        catch (TaskLengthLimitException) {}
+        
+        catch (DuplicateTaskException) {}
+
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла непредвиденная ошибка: " +
+                              $"{e.GetType()}: {e.Message} \n {e.StackTrace} \n {e.InnerException}");
+        }
     }
 
     public static void StartApp()
     {
+        Console.Write("Введите максимально допустимое количество задач (от 1 до 100): ");
+        taskCountLimit = ParseAndValidateInt(Console.ReadLine(), 1, 100);
+        
+        if (taskCountLimit <= 0 || taskCountLimit > 100) 
+            throw new ArgumentException("Максимально допустимое количество задач должно быть в диапазоне от 1 до 100");
+        
+        Console.Write("Введите максимально допустимую длину задачи: ");
+        taskLengthLimit = ParseAndValidateInt(Console.ReadLine(), 1, 100);
+        
         do
         {
             Console.Write("Введите свое имя: ");
             User.TelegramUserName = Console.ReadLine();
+            ValidateString(User.TelegramUserName);
+            
         } while (string.IsNullOrWhiteSpace(User.TelegramUserName));
 
         Console.WriteLine($"Здравствуйте {User.TelegramUserName}.");
@@ -122,6 +155,9 @@ class Program
 
     public static void AddTask()
     {
+        if (todoList.Count > taskCountLimit && taskCountLimit > 0) 
+            throw new TaskCountLimitException(taskCountLimit);
+        
         string taskText = "";
 
         do
@@ -129,7 +165,15 @@ class Program
             Console.WriteLine("Введите описание задачи: ");
             taskText = Console.ReadLine();
         } while (string.IsNullOrWhiteSpace(taskText));
+        
+        ValidateString(taskText);
 
+        if (taskText.Length > taskLengthLimit && taskLengthLimit > 0) 
+            throw new TaskLengthLimitException(taskText.Length,  taskLengthLimit);
+        
+        if (todoList.Any(item => item.Name == taskText))
+            throw new DuplicateTaskException(taskText);
+        
         todoList.Add(new ToDoItem(User, taskText));
         Console.WriteLine("\nЗадача добавлена.");
     }
@@ -142,6 +186,8 @@ class Program
         {
             Console.Write("\nВведите Id задачи для завершения: ");
             taskId = Console.ReadLine();
+            
+            ValidateString(taskId);
 
             for (int i = 0; i < todoList.Count; i++)
             {
@@ -153,5 +199,22 @@ class Program
                 }
             }
         }
+    }
+
+    public static int ParseAndValidateInt(string? str, int min, int max)
+    {
+        int number;
+        int.TryParse(str, out number);
+        
+        if (number < min || number > max)
+            throw new ArgumentException($"Строка не является числом, либо выходит за пределы указанного диапазона от {min} до {max}.");
+        
+        return number;
+    }
+    
+    public static void ValidateString(string? str)
+    {
+        if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str)) 
+            throw new ArgumentException("Введенное значение не должно быть равно null, пустой строке или пробелу.");
     }
 }
